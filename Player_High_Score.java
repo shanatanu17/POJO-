675b2cdcbcfda95bf2ac4b09a5d42aa915c9be8a
shantanu17 <shantanushinde3292@gmail.com>
2025-10-01 07:14:59 +0530

    first commit

--- Player_High_Score.java
+++ Player_High_Score.java
@@ -1,5 +1,4 @@
 /*
-
 4. Player High Score
 Task:
  Create a Player class with fields: name and score.
@@ -8,10 +7,7 @@ Create two individual Player objects.
 Compare their scores and print the name of the player with the higher score.
 Explanation:
  This helps to understand comparing fields between objects.
-
 */
-
-
 
 import java.util.*;
 
@@ -20,9 +16,13 @@ class Player
   private String name;
   private int score;
 
+
+
   //setters and getters
+
+
   public void setname(String name)
-   { 
+   {
      this.name = name;
    }
 
@@ -31,70 +31,94 @@ class Player
      return name;
    }
 
+  
   public void setscore(int score)
    { 
-     this.score = score;
-   }  
-
-  public int getscore()
-   { 
-     return score;
+      this.score = score;
    }
+
+ public int getscore()
+  { 
+    return score;
+  }
+
 
 }
 
 
 
-
-public class Player_High_Score
+public class  Player_High_Score
 {
   public static void main(String ss[])
-   {
-      Player pl[] = new Player[2];
+   { 
+    
+     Player pl[] = new Player[2];
 
-      for(int i=0;i<pl.length;i++)
-       { 
+
+     // 1 . set the values for player in respective objects..
+
+     for(int i=0;i<pl.length;i++)
+      { 
          pl[i] = new Player();
 
          Scanner sc = new Scanner(System.in);
-         
-         System.out.println("Enter the information of player ");
-         String name  = sc.nextLine(); 
-         int score  = sc.nextInt();
- 
-         // now set it in repective object
 
-         pl[i].setname(name);
-         pl[i].setscore(score);
-       }
+         System.out.println("Enter the player" + (i+1)  + "  information ");
+
+         System.out.println("Enter name and score for player");
+         pl[i].setname(sc.nextLine()); 
+         pl[i].setscore(sc.nextInt());
+      }
 
 
- 
-       //Compare their scores and print the name of the player with the higher score
+  
+       //2 . Compare their scores and print the name of the player with the higher score.
 
-       
-       
-       if(pl[0].getscore() > pl[1].getscore() )
-        {  
-           System.out.println("This player has the highest score ");
-           String name = pl[0].getname();
-           System.out.println(name);
-        } 
-       else if(pl[0].getscore() < pl[1].getscore())
+      int maxscore = Integer.MIN_VALUE;
+
+      String n = " ";
+      int s = 0;
+
+
+       for(int i=0;i<pl.length;i++)
         { 
-           System.out.println("This player has the highest score ");
-           String name = pl[1].getname();
-           System.out.println(name);
+           if(pl[i].getscore() > maxscore)
+            { 
+               maxscore = pl[i].getscore();
+               n = pl[i].getname();
+               s = pl[i].getscore();
+            }
         }
-      else
-        { 
-           System.out.println("Both players have the same score ");
-        }
-      
+
+ 
+       System.out.println("Information of the player who has the maximum score");
+
+       System.out.print(" name = " +  n  + " score = " + s);
+       
+
+  
+
  
  
+    
    }
 
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
